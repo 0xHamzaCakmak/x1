d5cb93f86b80357b08d3dc6bb3d06394fc0edc24
0xhamzacakmak <reelaktor@gmail.com>
2022-08-11 00:39:55 +0300

    design

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Button} from "react-native";
-
+import { ethers } from "ethers";
 export default function App() {
   const [errorMessage, setErrorMessage] = useState(null);
 	const [defaultAccount, setDefaultAccount] = useState(null);
@@ -15,8 +15,15 @@ const accountChangeHandler = (account) =>{
 
   const ConnectWallet = () =>{
     if(window.ethereum){
-     window.ethereum.request({method:'eth_requestAccounts'},[1, 2, 3, 4, 42,56]).then((result)=>{
+     window.ethereum.request({method:'eth_requestAccounts',  params: [{ chainId: '0x38' }]}).then((result)=>{
        accountChangeHandler(result[0]);
+       window.ethereum.request({method: 'eth_getBalance', params: [result[0], 'latest']})
+		.then(balance => {
+			setUserBalance(ethers.utils.formatEther(balance));
+		})
+		.catch(error => {
+			setErrorMessage(error.message);
+		});
      })
     }
     else{
@@ -24,10 +31,16 @@ const accountChangeHandler = (account) =>{
       alert("Please Install Metamask")
     }
   }
+	const chainChangedHandler = () => {
+		// reload the page to avoid any errors with chain change mid use of application
+		window.location.reload();
+	}
+  window.ethereum.on('accountsChanged', accountChangeHandler);
+  window.ethereum.on('chainChanged', chainChangedHandler);
   return (
     <View style={styles.container}>
       <Button onPress={()=>ConnectWallet()} title="Connect Wallet"></Button>
-      {defaultAccount !=undefined? <Text>Address is :  {defaultAccount}</Text> : null}
+      {defaultAccount !=undefined? <Text>Address is :  {defaultAccount} </Text> : null}
       <StatusBar style="auto" />
     </View>
   );
